33cdd69e888138d523bf688cd39960096d0e8e8b
imac1 <irina.macovei@yahoo.fr>
2023-01-18 21:50:01 +0200

    toggle task statuses

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // 1. add a task and clear the input fields
 
 document.getElementById("addButton").addEventListener("click", function () {
@@ -17,3 +16,26 @@ document.getElementById("addButton").addEventListener("click", function () {
 });
 
 // 2. toggle the status of a task
+
+// get the list container
+let list = document.getElementById("list");
+
+// add an event listener to the list container
+list.addEventListener("click", function (event) {
+	// check if the event target is a toggleStatus button
+	if (event.target.classList.contains("toggleStatus")) {
+		// toggle the done class on the parent div
+		event.target.parentElement.classList.toggle("done");
+
+		// toggle the button text
+		if (event.target.innerHTML === "done") {
+			event.target.innerHTML = "not done";
+		} else {
+			event.target.innerHTML = "done";
+		}
+	}
+});
+
+// 3. Pressing the `Complete all` button will set all tasks to done
+
+
